365de2c7bb5184a2022656155e1ad6272e42d417
Sebastian Inman <hello@sebastiancodes.com>
2025-12-04 15:02:50 -0800

    patch(components/icon): add icon size properties

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,9 +1,12 @@
 import { getIconComponent, type IconName } from "./icons";
-import { use, Suspense} from "react";
+import { use, useMemo, Suspense} from "react";
 import "./icon.css";
 
 export type IconProps = Omit<React.SVGProps<SVGSVGElement>, "aria-hidden"> & {
-    name: IconName;
+    "name": IconName;
+    "size"?: `${number}${string}`;
+    "size:md"?: `${number}${string}`;
+    "size:lg"?: `${number}${string}`;
 }
 
 function IconComponent({ name, ...props }: IconProps) {
@@ -12,9 +15,19 @@ function IconComponent({ name, ...props }: IconProps) {
 }
 
 export function Icon({ name, ...props }: IconProps) {
-    return (<div ui-icon={name}>
+    const { size = "1rem", "size:md": sizeMd, "size:lg": sizeLg, ...rest } = props;
+
+    const styleVariables = useMemo(() => {
+        const styles = new Map<string, string>();
+        size && styles.set("--icon-size-base", size);
+        sizeMd && styles.set("--icon-size-md", sizeMd);
+        sizeLg && styles.set("--icon-size-lg", sizeLg);
+        return styles.size ? Object.fromEntries(styles) : undefined;
+    }, [size, sizeMd, sizeLg]);
+
+    return (<div ui-icon={name} style={styleVariables}>
         <Suspense fallback={null}>
-            <IconComponent name={name} {...props} />
+            <IconComponent name={name} {...rest} />
         </Suspense>
     </div>);
 }
