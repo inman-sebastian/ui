6d7e5766b7c29fa75287bfa2be752df53ad94705
Sebastian Inman <hello@sebastiancodes.com>
2025-12-04 14:31:52 -0800

    chore(components/icon): add javascript logo icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,6 @@
 const icons = {
-    external: import("./icon-external").then(module => module.IconExternal),
+    "external": import("./icon-external").then(module => module.IconExternal),
+    "javascript-logo": import("./icon-javascript-logo").then(module => module.IconJavascriptLogo)
 } as const;
 
 export type IconName = keyof typeof icons;
