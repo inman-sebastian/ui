0b61328add3a6cb2d26139a18666fec3b4ebf4e6
Sebastian Inman <hello@sebastiancodes.com>
2025-12-04 14:47:33 -0800

    feat(components/icon): add nodejs logo icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,7 @@
 const icons = {
     "external": import("./icon-external").then(module => module.IconExternal),
     "javascript-logo": import("./icon-javascript-logo").then(module => module.IconJavaSriptLogo),
+    "nodejs-logo": import("./icon-nodejs-logo").then(module => module.IconNodeJSLogo),
     "typescript-logo": import("./icon-typescript-logo").then(module => module.IconTypeScriptLogo)
 } as const;
 
