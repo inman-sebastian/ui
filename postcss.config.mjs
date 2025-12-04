10145fa39d504726b17633eeb40d769298ed1d7b
Sebastian Inman <hello@sebastiancodes.com>
2025-12-04 13:50:34 -0800

    build: use js instead of ts for postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,8 @@
-import type { Config } from "postcss-load-config";
 import postcssImport from "postcss-import";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 
-const config: Config = {
+/** @type {import("postcss-load-config").Config } */
+export default {
     plugins: [postcssImport(), autoprefixer(), cssnano()]
 };
-
-export default config;
